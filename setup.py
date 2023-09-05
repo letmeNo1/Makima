78788a88d2a5cac0b89ffb798c3d64dfed1e2242
hank.huang <44250052+letmeNo1@users.noreply.github.com>
2023-09-05 10:58:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def parse_requirements(filename):
 
 setuptools.setup(
     name="Makima",
-    version="0.1.9.1",
+    version="0.1.9.2",
     author="hank.huang",
     author_email="hank.huang550@gmail.com",
     description="A cross-platform desktop automated testing framework.",
